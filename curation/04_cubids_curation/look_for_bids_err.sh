d5c50d4b278d0e3860c70d6c5df9dd8e9a339462
Parker Singleton <sps253@cornell.edu>
2025-03-04 12:50:40 -0500

    typo

--- curation/04_cubids_curation/look_for_bids_err.sh
+++ curation/04_cubids_curation/look_for_bids_err.sh
@@ -15,7 +15,7 @@
 for subdir in sub-*; do
   anat_dir="$subdir/ses-1/func"
   if [ -d "$anat_dir" ]; then
-    has_run01=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_task-face_acq-singleband_bold.json' | wc -l)
+    has_run01=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_run-01_task-face_acq-singleband_bold.json' | wc -l)
     has_run02=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_run-02_task-face_acq-singleband_bold.json' | wc -l)
 
     if [[ "$has_run01" -gt 0 && "$has_run02" -eq 0 ]]; then
