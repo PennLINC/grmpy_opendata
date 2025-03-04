fb5da148e0f5d11614e5a080613f50740be488f7
Parker Singleton <sps253@cornell.edu>
2025-03-04 12:45:13 -0500

    face task

--- curation/04_cubids_curation/look_for_bids_err.sh
+++ curation/04_cubids_curation/look_for_bids_err.sh
@@ -1,8 +1,8 @@
 for subdir in sub-*; do
-  anat_dir="$subdir/ses-1/anat"
+  anat_dir="$subdir/ses-1/func"
   if [ -d "$anat_dir" ]; then
-    has_run01=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_run-01_angio.json' | wc -l)
-    has_standard=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_angio.json' | wc -l)
+    has_run01=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_run-01_task-face_acq-singleband_bold.json' | wc -l)
+    has_standard=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_task-face_acq-singleband_bold.json' | wc -l)
 
     if [[ "$has_run01" -gt 0 && "$has_standard" -gt 0 ]]; then
       echo "Found both in: $subdir"
