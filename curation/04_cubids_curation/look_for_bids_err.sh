f5a3ed6d966725f774a895c63b6ef0f8c60153f1
Parker Singleton <sps253@cornell.edu>
2025-03-04 12:48:59 -0500

    run01 but not run02

--- curation/04_cubids_curation/look_for_bids_err.sh
+++ curation/04_cubids_curation/look_for_bids_err.sh
@@ -1,11 +1,26 @@
+# look for cases where there is a standard scan and a run-01 scan
+# for subdir in sub-*; do
+#   anat_dir="$subdir/ses-1/func"
+#   if [ -d "$anat_dir" ]; then
+#     has_run01=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_run-01_task-face_acq-singleband_bold.json' | wc -l)
+#     has_standard=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_task-face_acq-singleband_bold.json' | wc -l)
+
+#     if [[ "$has_run01" -gt 0 && "$has_standard" -gt 0 ]]; then
+#       echo "Found both in: $subdir"
+#     fi
+#   fi
+# done
+
+# look for cases where there is a run-01 but no run-01
 for subdir in sub-*; do
   anat_dir="$subdir/ses-1/func"
   if [ -d "$anat_dir" ]; then
-    has_run01=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_run-01_task-face_acq-singleband_bold.json' | wc -l)
-    has_standard=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_task-face_acq-singleband_bold.json' | wc -l)
+    has_run01=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_task-face_acq-singleband_bold.json' | wc -l)
+    has_run02=$(find "$anat_dir" -maxdepth 1 -name '*_ses-1_run-02_task-face_acq-singleband_bold.json' | wc -l)
 
-    if [[ "$has_run01" -gt 0 && "$has_standard" -gt 0 ]]; then
-      echo "Found both in: $subdir"
+    if [[ "$has_run01" -gt 0 && "$has_run02" -eq 0 ]]; then
+      echo "Found run-01 but NO run-02 in: $subdir"
     fi
   fi
 done
+
