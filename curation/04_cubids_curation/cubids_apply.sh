717734aea6a0b0c5d331a52f1e9fe8f2d3e9a1ef
Parker Singleton <sps253@cornell.edu>
2025-03-10 21:34:46 -0400

    cubids apply

--- curation/04_cubids_curation/cubids_apply.sh
+++ curation/04_cubids_curation/cubids_apply.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=cubids_apply
-#SBATCH --output=cubids_apply_%A.out
-#SBATCH --error=cubids_apply_%A.err
+#SBATCH --output=/cbica/projects/grmpy/code/curation/04_cubids_curation/v5/cubids_apply_%A.out
+#SBATCH --error=/cbica/projects/grmpy/code/curation/04_cubids_curation/v5/cubids_apply_%A.err
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --time=48:00:00        # Adjust time as needed
 #SBATCH --mem=64G
 
-# Add mamba environment executables to path
+# Add mamba environment executables to path ### CHECK v# in all paths below and in our/err outpaths!!!
 source /cbica/projects/grmpy/.bash_profile
 PATH=$PATH:/cbica/projects/grmpy/miniforge3/envs/cubids/bin/
 PATH=$PATH:/cbica/projects/grmpy/miniforge3/condabin/
 PATH=$PATH:/cbica/projects/grmpy/data/bids_datalad/
-PATH=$PATH:/cbica/projects/grmpy/data/bids_datalad/code/CuBIDS/
+PATH=$PATH:/cbica/projects/grmpy/code/curation/04_cubids_curation/v5/
 mamba activate cubids
 
 # Run CuBIDS apply
-cd /cbica/projects/grmpy/data/bids_datalad/code/CuBIDS/
-/cbica/projects/grmpy/miniforge3/envs/cubids/bin/cubids apply /cbica/projects/grmpy/data/bids_datalad /cbica/projects/grmpy/data/bids_datalad/code/CuBIDS/v1_summary.tsv /cbica/projects/grmpy/data/bids_datalad/code/CuBIDS/v1_files.tsv v2 --use-datalad
+cd /cbica/projects/grmpy/data/bids_datalad/
+/cbica/projects/grmpy/miniforge3/envs/cubids/bin/cubids apply /cbica/projects/grmpy/data/bids_datalad /cbica/projects/grmpy/code/curation/04_cubids_curation/v4/v4_summary_edited.tsv /cbica/projects/grmpy/code/curation/04_cubids_curation/v4/v4_files.tsv /cbica/projects/grmpy/code/curation/04_cubids_curation/v5/v5 --use-datalad
