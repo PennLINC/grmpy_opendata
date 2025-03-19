35d1a8a8f6090bd3d95a0f39c0102b0b374e059a
Parker Singleton <sps253@cornell.edu>
2025-03-19 16:35:40 -0400

    fix path

--- curation/05_babs/babs_init.sh
+++ curation/05_babs/babs_init.sh
@@ -3,9 +3,9 @@ cd ${project_root}/data/BABS/exemplars/derivatives
 babs-init \
      --where_project ${PWD} \
      --project_name fmriprepANAT \
-     --input BIDS ${project_root}/data/BABS/exemplars/BIDS \
+     --input BIDS ${project_root}/data/exemplars/ \
      --container_ds ${project_root}/data/BABS/apptainer-datasets/fmriprep-24-1-1-ds \
      --container_name fmriprep-24-1-1 \
-     --container_config_yaml_file ${project_root}/code/curation/05_babs/config_fmriprepANAT.yaml \
+     --container_config_yaml_file ${project_root}/code/curation/05_babs/config_fmriprep_anat.yaml \
      --type_session single-ses \
      --type_system slurm
