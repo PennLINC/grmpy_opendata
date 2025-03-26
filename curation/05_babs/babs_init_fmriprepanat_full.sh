b0dabfe46ec578aa563053c954582c278b54995d
Parker Singleton <sps253@cornell.edu>
2025-03-26 12:57:56 -0400

    babs init shells

---
+++ curation/05_babs/babs_init_fmriprepanat_full.sh
@@ -0,0 +1,10 @@
+project_root=/cbica/projects/grmpy
+cd ${project_root}/data/BABS/derivatives
+babs init \
+     ${PWD}/fmriprepANAT \
+     --datasets BIDS=${project_root}/data/bids_datalad/ \
+     --container-ds ${project_root}/data/BABS/apptainer-datasets/fmriprep-24-1-1-ds \
+     --container-name fmriprep-24-1-1 \
+     --container-config ${project_root}/code/curation/05_babs/fmriprep-24-1-1_anatonly.yaml \
+     --processing-level subject \
+     --queue slurm
