d6783cc7fe3cdc156496af1b8a614a8d57883ecd
Parker Singleton <sps253@cornell.edu>
2025-04-03 13:25:48 -0400

    still working

--- curation/05_babs/babs_init_fmriprep-ingressed.sh
+++ curation/05_babs/babs_init_fmriprep-ingressed.sh
@@ -3,8 +3,8 @@ cd ${project_root}/data/BABS/derivatives
 export TEMPLATEFLOW_HOME=${PWD}/templateflow_home
 mkdir -p ${TEMPLATEFLOW_HOME}
 babs init \
-     ${PWD}/fmriprep \
-     --datasets BIDS=${project_root}/data/bids_datalad/ fmriprep_anat=${project_root}/derivatives/fmriprep_anat \
+     ${PWD}/fmriprep_func \
+     --datasets BIDS=${project_root}/data/bids_datalad/ fmriprep_anat=${project_root}/derivatives/fmriprepANAT \
      --container-ds ${project_root}/data/BABS/apptainer-datasets/fmriprep-25-0-0-ds \
      --container-name fmriprep-25-0-0 \
      --container-config ${project_root}/code/curation/05_babs/fmriprep-25-0-0_ingressed-fs.yaml \
