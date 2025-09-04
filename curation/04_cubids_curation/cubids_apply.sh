aa5c739e49253322f58a195756d7c0dea70123b3
Parker Singleton <sps253@cornell.edu>
2025-09-04 16:04:07 -0400

    clean up

--- curation/04_cubids_curation/cubids_apply.sh
+++ curation/04_cubids_curation/cubids_apply.sh
@@ -7,12 +7,7 @@
 #SBATCH --time=48:00:00        # Adjust time as needed
 #SBATCH --mem=64G
 
-# Add mamba environment executables to path ### CHECK v# in all paths below and in our/err outpaths!!!
-# source /cbica/projects/grmpy/.bash_profile
-# PATH=$PATH:/cbica/projects/grmpy/miniforge3/envs/cubids/bin/
-# PATH=$PATH:/cbica/projects/grmpy/miniforge3/condabin/
-PATH=$PATH:/cbica/projects/grmpy/data/bids_datalad/
-PATH=$PATH:/cbica/projects/grmpy/code/curation/04_cubids_curation/v3/
+#XXX: CHECK v# in all paths below and in our/err outpaths!!!
 
 micromamba activate cubids
 
