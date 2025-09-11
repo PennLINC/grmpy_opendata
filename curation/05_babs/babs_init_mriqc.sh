d2294d7c8047be4df8c4bbc891ad92a930d97b06
Parker Singleton <sps253@cornell.edu>
2025-09-11 15:13:01 -0400

    new babs args changed

--- curation/05_babs/babs_init_mriqc.sh
+++ curation/05_babs/babs_init_mriqc.sh
@@ -4,9 +4,9 @@ export TEMPLATEFLOW_HOME=${PWD}/templateflow_home
 mkdir -p ${TEMPLATEFLOW_HOME}
 babs init \
      ${PWD}/mriqc \
-     --datasets BIDS=${project_root}/data/bids_datalad/ \
      --container-ds ${project_root}/data/BABS/apptainer-datasets/mriqc-24-0-0rc0-ds \
      --container-name mriqc-24-0-0rc0 \
      --container-config ${project_root}/code/curation/05_babs/mriqc-24-0-0rc0.yaml \
      --processing-level subject \
      --queue slurm
+     ${project_root}/data/bids_datalad/
