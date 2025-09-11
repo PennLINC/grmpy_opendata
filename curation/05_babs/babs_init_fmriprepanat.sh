b7e07fca23e0efa32cc20afb43ce83178cfc7423
Parker Singleton <sps253@cornell.edu>
2025-09-11 15:37:08 -0400

    update fmriprep versions and new babs configurations

--- curation/05_babs/babs_init_fmriprepanat.sh
+++ curation/05_babs/babs_init_fmriprepanat.sh
@@ -3,10 +3,9 @@ cd ${project_root}/data/BABS/derivatives
 export TEMPLATEFLOW_HOME=${PWD}/templateflow_home
 mkdir -p ${TEMPLATEFLOW_HOME}
 babs init \
-     ${PWD}/fmriprepANAT \
-     --datasets BIDS=${project_root}/data/bids_datalad/ \
-     --container-ds ${project_root}/data/BABS/apptainer-datasets/fmriprep-24-1-1-ds \
-     --container-name fmriprep-24-1-1 \
-     --container-config ${project_root}/code/curation/05_babs/fmriprep-24-1-1_anatonly.yaml \
+     --container-ds ${project_root}/data/BABS/apptainer-datasets/fmriprep-25-1-4-ds \
+     --container-name fmriprep-25-1-4 \
+     --container-config ${project_root}/code/curation/05_babs/fmriprep-25-1-4_anatonly.yaml \
      --processing-level subject \
      --queue slurm
+     "${project_root}/data/bids_datalad/"
