79cd602892b95ac4c929162b66925d1347f72df6
Parker Singleton <sps253@cornell.edu>
2025-09-11 15:24:12 -0400

    correct mriqc version

--- curation/05_babs/babs_init_mriqc.sh
+++ curation/05_babs/babs_init_mriqc.sh
@@ -3,9 +3,9 @@ cd ${project_root}/data/BABS/derivatives
 export TEMPLATEFLOW_HOME=${PWD}/templateflow_home
 mkdir -p ${TEMPLATEFLOW_HOME}
 babs init \
-     --container-ds ${project_root}/data/BABS/apptainer-datasets/mriqc-24-0-0rc0-ds \
-     --container-name mriqc-24-0-0rc0 \
-     --container-config ${project_root}/code/curation/05_babs/mriqc-24-0-0rc0.yaml \
+     --container-ds ${project_root}/data/BABS/apptainer-datasets/mriqc-25-0-0rc0-ds \
+     --container-name mriqc-25-0-0rc0 \
+     --container-config ${project_root}/code/curation/05_babs/mriqc-25-0-0rc0.yaml \
      --processing-level subject \
      --queue slurm \
      "${project_root}/data/BABS/derivatives/mriqc"
