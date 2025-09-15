bd62ce73343a08cf12155fb10e30839a146c8f04
Parker Singleton <sps253@cornell.edu>
2025-09-15 15:00:36 -0400

    aslprep init

---
+++ curation/05_babs/babs_init_aslprep.sh
@@ -0,0 +1,11 @@
+project_root=/cbica/projects/grmpy
+cd ${project_root}/data/BABS/derivatives
+export TEMPLATEFLOW_HOME=${PWD}/templateflow_home
+mkdir -p ${TEMPLATEFLOW_HOME}
+babs init \
+     --container-ds ${project_root}/data/BABS/apptainer-datasets/aslprep-25-0-0-ds \
+     --container-name aslprep-25-0-0 \
+     --container-config ${project_root}/code/curation/05_babs/aslprep-25-0-0.yaml \
+     --processing-level subject \
+     --queue slurm \
+     "${project_root}/data/BABS/derivatives/aslprep"
